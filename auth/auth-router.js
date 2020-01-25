51c92dfd9792cfd17bffb03030831bc2d4830898
Clif <clifhodges13@gmail.com>
2020-01-25 11:30:52 -0600

    implement register feature to save credentials to users db

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();
+const usersModel = require('../users/usersModel')
 
-router.post('/register', (req, res) => {
+const errorMessage = { 
+  message: 'You need to include a valid username and password in your request.'
+}
+
+router.post('/register', async (req, res, next) => {
   // implement registration
-  
+  const user = req.body
+
+  if (!user || !user.username || !user.password) {
+    return res.status(401).json(errorMessage)
+  }
+
+  try {
+    const saved = await usersModel.add(user)
+    res.status(201).json(saved)
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.post('/login', (req, res) => {
